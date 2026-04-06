9f2bec8dd108472ec994ea6c9d74a627b5269b4d
KarthikrajMoorthy <karthikrajmoorthy@gmail.com>
2026-04-06 16:02:43 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/officehours/MainActivity.kt
+++ app/src/main/java/com/example/officehours/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.officehours
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private var startTime: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val tv = TextView(this)
-        tv.text = "App Working ✅"
+        val statusText = findViewById<TextView>(R.id.statusText)
+        val startBtn = findViewById<Button>(R.id.startBtn)
+        val stopBtn = findViewById<Button>(R.id.stopBtn)
 
-        setContentView(tv)
+        startBtn.setOnClickListener {
+            startTime = System.currentTimeMillis()
+            statusText.text = "Office Started"
+        }
+
+        stopBtn.setOnClickListener {
+            val endTime = System.currentTimeMillis()
+            val duration = (endTime - startTime) / 1000
+            statusText.text = "Worked: $duration sec"
+        }
     }
 }
