48d40c3892a3b3ce439d16e5f02f1bb93beaadd3
KarthikrajMoorthy <karthikrajmoorthy@gmail.com>
2026-04-06 15:12:56 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/officehours/MainActivity.kt
+++ app/src/main/java/com/example/officehours/MainActivity.kt
@@ -5,11 +5,12 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val textView = TextView(this)
-        textView.text = "Office App Running"
+        textView.text = "Office App Running Successfully ✅"
 
         setContentView(textView)
     }
